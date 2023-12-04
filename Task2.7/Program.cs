6b5f03394a0eee2b60ddc5fd3de77f39b49001a1
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-04 21:23:06 +0200

    Добавьте файлы проекта.

---
+++ Task2.7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Введене число є парним");
+        }
+        else
+        {
+            Console.WriteLine("Введене число є непарним");
+        }
+        if (number % 3 == 0)
+        {
+            Console.WriteLine("Введене число ділиться на 3");
+        }
+        else
+        {
+            Console.WriteLine("Введене число не ділиться на 3");
+        }
+    }
+}
